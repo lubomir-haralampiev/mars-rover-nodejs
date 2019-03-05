ded3ca9e8e887c3e59ae28eb1ff206474e0d2231
Lubomir Haralampiev <lubomir.haralampiev@deutschebahn.com>
2019-03-05 18:36:00 +0100

    test the validation module

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,5 +1,7 @@
 import {clone as createExpect} from 'unexpected';
 import unexpectedSinon from 'unexpected-sinon';
 
-export const expect = createExpect(); // eslint-disable-line import/prefer-default-export
+export const expect = createExpect();
 expect.use(unexpectedSinon);
+
+export const randomInteger = () => Math.round(Math.random() * 100);
